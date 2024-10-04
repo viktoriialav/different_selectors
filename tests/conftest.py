b212e25c9336111fd448d6545568f30590cb2ed3
Viktoriia Lavrova <viktoriia.lav2908@gmail.com>
2024-10-04 23:57:40 +0300

    Update tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,15 @@
 import pytest
 from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.webdriver import WebDriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 import config
 
-@pytest.fixture(scope='function', autouse=True)
-def browser_management():
+
+@pytest.fixture(scope='function')
+def browser_management_selene():
     driver_options = config.settings.driver_options
 
     browser.config.base_url = config.settings.base_url
@@ -16,3 +21,17 @@ def browser_management():
     yield
 
     browser.quit()
+
+
+@pytest.fixture(scope='function')
+def browser_management_selenium():
+    driver_options = config.settings.driver_options
+
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),
+                              options=driver_options)
+    driver.set_window_size(width=config.settings.window_width,
+                           height=config.settings.window_height)
+
+    yield driver
+
+    driver.quit()
--- tests/test_litecart_xpath.py
+++ tests/test_litecart_xpath.py
@@ -1,17 +1,21 @@
 from selene import browser, query, have, be
 
 
-class TestLiteCart:
-    def test_1(self):
-        # 1. Подберите локатор для поиска на странице http://litecart.stqa.ru/index.php/en/ всех блоков (li) с информацией
-        # о товарах (каждому товару соответствует свой блок)
+class TestLiteCartUsingXPath:
+    def test_1(self, browser_management_selene):
+        '''
+        1. Подберите локатор для поиска на странице http://litecart.stqa.ru/index.php/en/ всех блоков (li) с информацией
+        о товарах (каждому товару соответствует свой блок)
+        '''
         browser.open('/')
         browser.all('//li[contains(@class,"product")]').should(have.size(11))
 
 
-    def test_2(self):
-        # 2. Подберите локатор для поиска на странице http://litecart.stqa.ru/index.php/en/ всех ссылок (a) на страницы
-        # товаров в основной части страницы (не считая боковых блоков)
+    def test_2(self, browser_management_selene):
+        '''
+        2. Подберите локатор для поиска на странице http://litecart.stqa.ru/index.php/en/ всех ссылок (a) на страницы
+        товаров в основной части страницы (не считая боковых блоков)
+        '''
         browser.open('/')
 
         all_links = browser.all('//li[contains(@class,"product")]/a[contains(@class,"link")]')
@@ -20,26 +24,31 @@ class TestLiteCart:
         browser.open(all_links[1].get(query.attribute('href')))
         browser.element('//button[@name="add_cart_product"]').should(be.clickable)
 
-    def test_3(self):
-        # 3. Подберите локатор для поиска на странице http://litecart.stqa.ru/index.php/en/ ссылки на Privacy Policy
-        # в нижней части страницы
+    def test_3(self, browser_management_selene):
+        '''
+        3. Подберите локатор для поиска на странице http://litecart.stqa.ru/index.php/en/ ссылки на Privacy Policy
+        в нижней части страницы
+        '''
         browser.open('/')
         link = browser.element('//*[@id="footer"]//a[contains(@href,"privacy-policy")]').get(query.attribute('href'))
         browser.open(link)
         browser.element('//h1').should(have.exact_text('Privacy Policy'))
 
-    def test_4(self):
-        # 4. Подберите локатор для поиска на странице http://litecart.stqa.ru/index.php/en/ всех элементов верхнего меню,
-        # находящихся на верхнем уровне (без элементов вложенных выпадающих меню)
+    def test_4(self, browser_management_selene):
+        '''
+        4. Подберите локатор для поиска на странице http://litecart.stqa.ru/index.php/en/ всех элементов верхнего меню,
+        находящихся на верхнем уровне (без элементов вложенных выпадающих меню)
+        '''
         browser.open('/')
         browser.all('//*[@id="site-menu"]/*/li').should(have.size(2))
 
-    def test_5_6(self):
-        # 5, 6 United States
-        # 5. Подберите локатор для поиска на странице http://litecart.stqa.ru/index.php/en/create_account элемента
-        # с текстом United States из выпадающего списка стран
-        # 6. На странице http://litecart.stqa.ru/index.php/en/create_account выберите страну United States и подберите
-        # локатор для выпадающего списка штатов
+    def test_5_6(self, browser_management_selene):
+        '''
+        5. Подберите локатор для поиска на странице http://litecart.stqa.ru/index.php/en/create_account элемента
+        с текстом United States из выпадающего списка стран
+        6. На странице http://litecart.stqa.ru/index.php/en/create_account выберите страну United States и подберите
+        локатор для выпадающего списка штатов
+        '''
         browser.open('/create_account')
 
         browser.element('//span[contains(@id,"select2-country_code")]').click()
@@ -49,9 +58,11 @@ class TestLiteCart:
 
         browser.element('//span[contains(@id,"select2-country_code")]').should(have.exact_text('United States'))
 
-    def test_7(self):
-        # 7. Подберите локатор для поиска на странице http://litecart.stqa.ru/index.php/en/acme-corp-m-1/ кнопки
-        # сортировки товаров по дате
+    def test_7(self, browser_management_selene):
+        '''
+        7. Подберите локатор для поиска на странице http://litecart.stqa.ru/index.php/en/acme-corp-m-1/ кнопки
+        сортировки товаров по дате
+        '''
         browser.open('/acme-corp-m-1/')
 
         browser.element('//a[contains(@href,"sort=date")]').click()
@@ -59,27 +70,33 @@ class TestLiteCart:
         browser.element('//span[contains(@class, "button active")]').should(have.exact_text('Date'))
 
 
-    def test_8(self):
-        # 8.Подберите локатор для поиска на странице http://litecart.stqa.ru/index.php/en/acme-corp-m-1/ иконки-лупы
-        # для увеличения картинки товара, имеющего стикер Sale
+    def test_8(self, browser_management_selene):
+        '''
+        8.Подберите локатор для поиска на странице http://litecart.stqa.ru/index.php/en/acme-corp-m-1/ иконки-лупы
+        для увеличения картинки товара, имеющего стикер Sale
+        '''
         browser.open('/acme-corp-m-1/')
 
         browser.element('//*[contains(@class, "sticker") and contains(@class, "sale")]/../../../*[contains(@class, "zoomable")]').click()
 
         browser.element('#fancybox-close').should(be.clickable)
 
-    def test_9(self):
-        # 9. Подберите локатор для поиска на странице http://litecart.stqa.ru/index.php/en/acme-corp-m-1/ всех ссылок на
-        # товары, у которых нет стикера Sale
+    def test_9(self, browser_management_selene):
+        '''
+        9. Подберите локатор для поиска на странице http://litecart.stqa.ru/index.php/en/acme-corp-m-1/ всех ссылок на
+        товары, у которых нет стикера Sale
+        '''
         browser.open('/acme-corp-m-1/')
 
         browser.all('//li[contains(@class, "product")][not(.//*[contains(@class, "sale")])]//a[contains(@class, "link")]').should(have.size(4))
 
 
-    def test_10(self):
-        # 10. Добавьте в магазине http://litecart.stqa.ru/index.php/en/ в корзину 2-3 товара, перейдите на страницу
-        # оформления заказа http://litecart.stqa.ru/index.php/en/checkout и подберите локатор для поиска элемента,
-        # содержащего общую сумму к оплате
+    def test_10(self, browser_management_selene):
+        '''
+        10. Добавьте в магазине http://litecart.stqa.ru/index.php/en/ в корзину 2-3 товара, перейдите на страницу
+        оформления заказа http://litecart.stqa.ru/index.php/en/checkout и подберите локатор для поиска элемента,
+        содержащего общую сумму к оплате
+        '''
         browser.open('/')
 
         # Добавляем первую утку
